4432de3932b54b07b6c423e71746d09702a8cddc
orlinkzz <humaedi.medi99@gmail.com>
2024-10-24 11:35:58 +0700

    fix: table session & seeder

--- migrations/20241024032512_create_sessions_table.cjs
+++ migrations/20241024032512_create_sessions_table.cjs
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('sessions', function(table) {
     table.increments('id').primary();
-    table.string('user_id').notNullable().unique();
+    table.integer('user_id').notNullable().unique();
     table.string('token').notNullable();
     table.timestamp('expires_at').nullable();
     table.timestamps(true, true);
--- seeds/seed_users.cjs
+++ seeds/seed_users.cjs
@@ -5,8 +5,7 @@
 exports.seed = async function(knex) {
   await knex('users').del();
   await knex('users').insert([
-    { email: 'test@example.com', first_name: 'example', last_name: '', password: 'hashedpassword' },
-    { email: 'test@test.com', first_name: 'test', last_name: '', password: 'hashedpassword' },
-    { email: 'senajhon@gmail.com', first_name: 'sena', last_name: 'jhon', password: 'hashedpassword' }
+    { email: 'example@test.com', first_name: 'example', last_name: 'test', password: '$2b$10$FvcKBDnjOV8Dm5TMUs7TC.8Ly2yzIqa/W4j/X47YF6lck4ozvTIoS' },
+    { email: 'senajhon@gmail.com', first_name: 'sena', last_name: 'jhon', password: '$2b$10$HoYG9KMdKNPr0pE5Me2ZXuF/Fz8Km3ZZ0RdHvzGDVdJ.tuu7AbyDS' }
   ]);
 };
